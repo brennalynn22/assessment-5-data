3f7fb74a0e3d2fcb574720da635487a2d2d3f607
Breezy Wentz <brwentz@gmail.com>
2022-07-27 23:42:00 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {SERVER_PORT} = process.env
+const {seed, getCountries, getCities, createCity, deleteCity, updateCities, addCities} = require('./controller.js')
+
+app.use(express.json())
+app.use(cors())
+
+// DEV
+app.post('/seed', seed)
+
+// COUNTRIES
+app.get('/countries', getCountries)
+
+// CITIES
+app.post('/cities', createCity)
+app.get('/cities', getCities)
+app.delete('/cities/:id', deleteCity)
+app.get('/cities', updateCities)
+app.post('/cities', addCities)
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
